101040b2c9e36ea1559fd81d71764bdf3d5298ec
grahamerics <76537201+grahamerics@users.noreply.github.com>
2020-12-23 16:12:12 -0600

    Update get-quote.py
    Removed #

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
 def main():
   # print("Keep it logically awesome.")
-
+print("Keep it logically awesome")
   #f = open("quotes.txt")
+  f=open("quotes.txt")
   #quotes = f.readlines()
+  quotes = f.readlines()
   #f.close()
-
+f = f.readlines()
   #print(quotes)
-
+print(quotes)
 if __name__== "__main__":
   main()
